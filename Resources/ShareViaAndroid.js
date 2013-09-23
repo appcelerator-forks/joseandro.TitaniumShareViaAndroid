542694b294bd5115dbe77e3030852282807f3e1b
Joseandro Luiz <eng.joseandro@gmail.com>
2013-09-23 05:07:50 -0300

    Localization fix

--- Resources/ShareViaAndroid.js
+++ Resources/ShareViaAndroid.js
@@ -1,147 +1,144 @@
 /*
-@Author: Joseandro Luiz 
-*/
+ @Author: Joseandro Luiz
+ */
 
-function ShareViaAndroid(msg, type, img){
+function ShareViaAndroid(msg, type, img) {
 	var dialog = Ti.UI.createAlertDialog({
-	    cancel: 2,
-	    buttonNames: ['Facebook', 'Outros Apps', 'Cancelar'],
-	    message: 'How would you like to share it?',
-	    title: 'Share'
+		cancel : 2,
+		buttonNames : ['Facebook', 'Other Apps', 'Cancel'],
+		message : 'How would you like to share it?',
+		title : 'Share'
 	});
-	
-	dialog.addEventListener('click', function(e){
-		if (e.index === e.source.cancel){
+
+	dialog.addEventListener('click', function(e) {
+		if (e.index === e.source.cancel) {
 			Ti.API.info('The cancel button was clicked');
-	    }
-		
-		switch(e.index){
+		}
+
+		switch(e.index) {
 			//Facebook
 			case 0:
 				var fb = require('facebook');
-				
+
 				fb.appid = "YOUR APP ID GOES HERE";
-				fb.permissions = ['publish_stream']; // Permissions your app needs
+				fb.permissions = ['publish_stream'];
+				// Permissions your app needs
 				fb.forceDialogAuth = true;
-				
+
 				fb.addEventListener('login', function(e) {
-				    if (e.success) {
-				    	if (type === "text"){
+					if (e.success) {
+						if (type === "text") {
 							// Now create the status message after you've confirmed that authorize() succeeded
-							fb.requestWithGraphPath('me/feed', {message: msg}, "POST", function(e) {
+							fb.requestWithGraphPath('me/feed', {
+								message : msg
+							}, "POST", function(e) {
 								if (e.success) {
-							        alert("Shared!");
-							    } 
-							    else {
+									alert("Shared!");
+								} else {
 									alert("Not shared =/");
-							    }
+								}
 							});
-				    	}
-				    	else{
+						} else {
 							// Now post the photo after you've confirmed that authorize() succeeded
 							// var f = Ti.Filesystem.getFile('pumpkin.jpg');
 							var blob = img.read();
-							
+
 							var data = {
-							    message: msg,
-							    picture: blob
+								message : msg,
+								picture : blob
 							};
-							
-							fb.requestWithGraphPath('me/photos', data, 'POST', function(e){
+
+							fb.requestWithGraphPath('me/photos', data, 'POST', function(e) {
 								if (e.success) {
-							        alert("Picture shared!");
-							    } 
-							    else {
+									alert("Picture shared!");
+								} else {
 									alert("Picture not shared =/");
-							    }
+								}
 							});
-				    	}
-				    } 
-				    else if (e.error) {
-				        alert("Try again! Error loggin in facebook : " + e.error);
-				        fb.logout();
-				    } 
-				    else if (e.cancelled) {
-				        // alert("Canceled");
-				    }
+						}
+					} else if (e.error) {
+						alert("Try again! Error loggin in facebook : " + e.error);
+						fb.logout();
+					} else if (e.cancelled) {
+						// alert("Canceled");
+					}
 				});
-				
+
 				if (!fb.loggedIn) {
 					fb.authorize();
-				}
-				else{
-			    	if (type === "text"){
+				} else {
+					if (type === "text") {
 						// Now create the status message after you've confirmed that authorize() succeeded
-						fb.requestWithGraphPath('me/feed', {message: msg}, "POST", function(e) {
+						fb.requestWithGraphPath('me/feed', {
+							message : msg
+						}, "POST", function(e) {
 							if (e.success) {
-						        alert("Shared!");
-						    } 
-						    else {
+								alert("Shared!");
+							} else {
 								alert("Not shared =/");
-						    }
+							}
 						});
-			    	}
-			    	else{
+					} else {
 						// Now post the photo after you've confirmed that authorize() succeeded
 						// var f = Ti.Filesystem.getFile('pumpkin.jpg');
 						var blob = img.read();
-						
+
 						var data = {
-						    message: msg,
-						    picture: blob
+							message : msg,
+							picture : blob
 						};
-						
-						fb.requestWithGraphPath('me/photos', data, 'POST', function(e){
+
+						fb.requestWithGraphPath('me/photos', data, 'POST', function(e) {
 							if (e.success) {
-						        alert("Picture shared!");
-						    } 
-						    else {
+								alert("Picture shared!");
+							} else {
 								alert("Picture not shared =/");
-						    }
+							}
 						});
-			    	}			
+					}
 				}
-			break;
-			
+				break;
+
 			case 1:
-				switch(type){
+				switch(type) {
 					case "image":
-					//To share images:
-					var share = Ti.Android.createIntent( { 
-					    action: Ti.Android.ACTION_SEND
-					});
-					share.type = 'image/jpeg';
-			
-					share.putExtra(Ti.Android.EXTRA_SUBJECT, "Share via Android");
-					share.putExtra(Ti.Android.EXTRA_TEXT, msg);
-					share.putExtraUri(Ti.Android.EXTRA_STREAM, img.getNativePath());
-					
-					Ti.Android.currentActivity.startActivity(Ti.Android.createIntentChooser(share, "Send Message"));
-					break;
-					
+						//To share images:
+						var share = Ti.Android.createIntent({
+							action : Ti.Android.ACTION_SEND
+						});
+						share.type = 'image/jpeg';
+
+						share.putExtra(Ti.Android.EXTRA_SUBJECT, "Share via Android");
+						share.putExtra(Ti.Android.EXTRA_TEXT, msg);
+						share.putExtraUri(Ti.Android.EXTRA_STREAM, img.getNativePath());
+
+						Ti.Android.currentActivity.startActivity(Ti.Android.createIntentChooser(share, "Send Message"));
+						break;
+
 					case "text":
 						//To share text
-						var share = Ti.Android.createIntent( { 
-						    action: Ti.Android.ACTION_SEND
+						var share = Ti.Android.createIntent({
+							action : Ti.Android.ACTION_SEND
 						});
 						share.type = 'text/plain';
 						share.addFlags(Ti.Android.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
 						share.putExtra(Ti.Android.EXTRA_SUBJECT, "Share via Android");
 						share.putExtra(Ti.Android.EXTRA_TEXT, msg);
 						share.setFlags(Ti.Android.FLAG_ACTIVITY_NEW_TASK);
-						
+
 						Ti.Android.currentActivity.startActivity(Ti.Android.createIntentChooser(share, "Send Message"));
-					break;
-					
+						break;
+
 					default:
-						alert("Sharing type not recognized!");		
+						alert("Sharing type not recognized!");
 				}
-			break;
-			
+				break;
+
 			default:
-				// alert('Cancelado');
+			// alert('Cancelado');
 		}
 	});
 	dialog.show();
 }
-module.exports = ShareViaAndroid;
+
+module.exports = ShareViaAndroid; 
